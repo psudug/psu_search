5820f1b25115a315a5f0b16dc237bfecfbe9e311
Michael Potter <mgp140@psu.edu>
2014-09-25 12:34:48 -0400

    Started redoing the css and js.

--- js/search_box.js
+++ js/search_box.js
@@ -1,13 +1,31 @@
 // Wrap in onReady function using jQuery. Don't assume jQuery is located at $.
 jQuery(document).ready(function($) {
 	// The element
-	var $ui = $('#ui_element');
+	var $ui = $('#block-psu-search-psu-search');
+	var $input = $('#block-psu-search-psu-search .form-type-textfield input');
+	var $options = $('#block-psu-search-psu-search .form-item-options');
+
 	// On focus and on click display the dropdown
-	$ui.find('.sb_input').bind('focus click', function($) {
-		$ui.find('.sb_dropdown').show();
+	$ui.find('.form-type-textfield input').bind('focus click', function($) {
+		$ui.find('.form-item-options').show();
 	});
 	// On mouse leave hide the dropdown
-	$ui.bind('mouseleave', function($) {
-		$ui.find('.sb_dropdown').hide();
+	$ui.bind('focusout', function($) {
+		hideOptions();
 	});
+
+	$('body').bind('click'), function($) {
+		hideOptions();
+	}
+
+	function hideOptions() {
+		if (!$input.infocus() && !$options.infocus()) {
+			$options.hide();
+			alert('true');
+		}
+		else {
+			alert('false');
+		}
+	}
+	hideOptions();
 });
