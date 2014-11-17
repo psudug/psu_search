fe49ef1dee5104fc3fa91f8782a5c269dac1cda6
Michael Potter <mgp140@psu.edu>
2014-11-17 14:14:31 -0500

    Added default styling and javascript interaction.

--- js/search_box.js
+++ js/search_box.js
@@ -1,31 +1,10 @@
 // Wrap in onReady function using jQuery. Don't assume jQuery is located at $.
 jQuery(document).ready(function($) {
-	// The element
-	var $ui = $('#block-psu-search-psu-search');
-	var $input = $('#block-psu-search-psu-search .form-type-textfield input');
-	var $options = $('#block-psu-search-psu-search .form-item-options');
-
-	// On focus and on click display the dropdown
-	$ui.find('.form-type-textfield input').bind('focus click', function($) {
-		$ui.find('.form-item-options').show();
-	});
-	// On mouse leave hide the dropdown
-	$ui.bind('focusout', function($) {
-		hideOptions();
+	$('#block-psu-search-psu-search').focusin(function() {
+		$(this).addClass('expanded');
 	});
 
-	$('body').bind('click'), function($) {
-		hideOptions();
-	}
-
-	function hideOptions() {
-		if (!$input.infocus() && !$options.infocus()) {
-			$options.hide();
-			alert('true');
-		}
-		else {
-			alert('false');
-		}
-	}
-	hideOptions();
+	$('#block-psu-search-psu-search').focusout(function() {
+		$(this).removeClass('expanded');
+	});
 });
